7f368874a6e0f0ddd895e6c6b1a822401d1967cd
Luis Sergio <62592632+LSCF84@users.noreply.github.com>
2025-11-28 15:33:14 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// IMPORTAR EL COMPONENTE DEL BANNER
+import CookieConsentBanner from './components/core/CookieConsentBanner'; 
 
 const inter = Inter({
     subsets: ["latin"],
@@ -25,6 +27,9 @@ export default function RootLayout({ children }) {
                 className={`${inter.variable} ${geistMono.variable} antialiased bg-gradient-to-b from-[#fafafc] via-[#e2e2e2] to-[#cbd5e1]`}
             >
                 {children}
+                
+                {/* AÑADIR EL BANNER DE COOKIES AL FINAL DEL BODY */}
+                <CookieConsentBanner />
             </body>
         </html>
     );
