b96fe62a302234fa2f4734135c13e1e3b5203c6c
Luis Sergio <62592632+LSCF84@users.noreply.github.com>
2025-11-29 17:10:22 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-    title: "Portfolio websteel",
+    title: "Portfolio Websteel",
     description:
         "Portfolio profesional de L.Sergio Camacho, Tecnico IT y desarrollador de software con experiencia en desarrollo web y aplicaciones Python.",
 };
@@ -35,4 +35,5 @@ export default function RootLayout({ children }) {
         </html>
     );
 }
+
 
